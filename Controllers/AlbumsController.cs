0fea06d10ff4e56c0ca44df0ebea17e20bc24f4b
Student <example@example.com>
2020-03-12 14:50:03 -0700

    add files to project begin bugtesting for broken artist info

---
+++ Controllers/AlbumsController.cs
@@ -0,0 +1,34 @@
+using Microsoft.AspNetCore.Mvc;
+using MusicOrganizer.Models;
+using System.Collections.Generic;
+
+namespace MusicOrganizer.Controllers
+{
+  public class AlbumsController: Controller
+  {
+    [HttpGet("/artists/{artistId}/albums/new")]
+    public ActionResult New(int albumId)
+    {
+      Artist artist = Artist.Find(albumId);
+      return View(artist);
+    }
+
+    [HttpPost("/albums/delete")]
+    public ActionResult DeleteAll()
+    {
+      Album.ClearAll(); //we are calling the method on the entire class Album, not just an instance
+      return View();
+    }
+
+    [HttpGet("/artists/{artistId}/albums/{albumId}")]
+    public ActionResult Show(int artistId, int albumId)
+    {
+      Album album = Album.Find(albumId); //calling the method on the Class Album
+      Artist artist = Artist.Find(artistId);
+      Dictionary<string, object> model = new Dictionary<string, object>();
+      model.Add("album", album);
+      model.Add("artist", artist);
+      return View(model);
+    }
+  }
+}
---
+++ Controllers/ArtistsController.cs
@@ -0,0 +1,54 @@
+using System.Collections.Generic;
+using System;
+using Microsoft.AspNetCore.Mvc;
+using MusicOrganizer.Models;
+
+namespace MusicOrganizer.Controllers
+{
+  public class ArtistsController: Controller
+  {
+    [HttpGet("/artists")]
+    public ActionResult Index()
+    {
+      List<Artist> allArtists = Artist.GetAll();
+      return View(allArtists);
+    }
+
+    [HttpGet("/artists/new")]
+    public ActionResult New()
+    {
+      return View();
+    }
+
+    [HttpPost("/artists")]
+    public ActionResult Create(string artistName)
+    {
+      Artist newArtist = new Artist(artistName);
+      return RedirectToAction("Index");
+    }
+
+    [HttpPost("/artists/{artistId}/albums")]
+    public ActionResult Create(int artistId, string albumTitle, string trackTotals, string runTime)
+    {
+      Dictionary<string, object> model = new Dictionary<string, object>();
+      Artist foundArtist = Artist.Find(artistId);
+      Album newAlbum = new Album(albumTitle, trackTotals, runTime);
+      foundArtist.AddAlbum(newAlbum);
+      List<Album> artistAlbums = foundArtist.Albums;
+      model.Add("albums", artistAlbums);
+      model.Add("artist", foundArtist);
+      return View("Show", model);
+    }
+
+    [HttpPost("/artists/{id}")]
+    public ActionResult Show(int id)
+    {
+      Dictionary<string, object> model = new Dictionary<string, object> ();
+      Artist selectedArtist = Artist.Find(id);
+      List<Album> artistAlbums = selectedArtist.Albums;
+      model.Add("artist", selectedArtist);
+      model.Add("albums", artistAlbums);
+      return View(model);
+    }
+  }
+}
