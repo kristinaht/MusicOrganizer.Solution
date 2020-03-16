56e7386136c0656a7fb6e7e8e61c76611484ca90
Kristina Hengster Tintor <hengster.tintor@gmail.com>
2020-03-16 14:28:35 -0700

    refactor Startup and add Database model
    Co-authored-by: Rachel Schieferstein <violenza@gmail.com>

---
+++ Models/Database.cs
@@ -0,0 +1,16 @@
+using System;
+using MySql.Data.MySqlClient;
+using MusicOrganizer;
+
+namespace MusicOrganizer.Models
+{
+  public class DB
+  {
+    public static MySqlConnection Connection()
+    {
+      MySqlConnection conn = new MySqlConnection(DBConfiguration.ConnectionString);
+      return conn;
+    }
+  }
+}
+
